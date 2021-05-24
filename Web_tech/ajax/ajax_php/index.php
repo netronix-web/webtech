c7d766c2ceff9815aa50d25e1750b69366310d61
Netronix <84571735+netronix-web@users.noreply.github.com>
2021-05-24 08:56:46 +0300

    Add files via upload

---
+++ Web_tech/ajax/ajax_php/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<script src="clienthint.js"></script>
+</head>
+<body>
+
+<form>
+<table width="100%" style="border:1px solid #d4d4d4;background-color:#e5eecc">
+<tr><td align="center">
+First Name: <input type="text" id="txt1" onkeyup="showHint(this.value)" />
+</form>
+<p>Suggestions: <span id="txtHint"></span></p>
+</td></tr></table>
+</body>
+</html>
